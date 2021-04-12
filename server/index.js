266f780f6a13d1821be1e2005fa4adebfc10a406
edelmutic <edelmutic@gmail.com>
2021-04-12 13:38:01 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const sequelize = require('./db');
 const models = require('./models/models');
+const cors = require('cors');
+const router = require('./routes/index');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('api', router);
 const port = process.env.PORT || 5000;
 
 const start = async () => {
---
+++ server/routes/userRouter.js
@@ -0,0 +1,8 @@
+const Router = require('express');
+const router = new Router();
+
+router.get('/auth', (req, res) => {});
+router.post('/login');
+router.post('/registration');
+
+module.exports = router;
