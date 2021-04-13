15bd8ac9aa7d21abc478738ea0f22ff7a4754a86
edelmutic <edelmutic@gmail.com>
2021-04-13 13:12:05 +0300

    add static

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('api', router);
-const port = process.env.PORT || 5000;
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+
+// Обработка ошибок, последний Middleware
+app.use(errorHandler);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(port, () => console.log(`Server Started on ${port}!`));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 const Router = require('express');
 const router = new Router();
+const deviceRouter = require('./deviceRouter');
 const userRouter = require('./userRouter');
 const brandRouter = require('./brandRouter');
-const deviceRouter = require('./deviceRouter');
 const typeRouter = require('./typeRouter');
 
 router.use('/user', userRouter);
