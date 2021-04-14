559b3224e2c449819c7a592d9f8ba101e0a5dcd7
edelmutic <edelmutic@gmail.com>
2021-04-14 10:51:48 +0300

    add store

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/consts';
+import { Context } from '../index';
+import { observer } from 'mobx-react-lite';
 
-const AppRouter = () => {
-  const isAuth = false;
+const AppRouter = observer(() => {
+  const { user } = useContext(Context);
+
+  console.log(user);
   return (
     <Switch>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, Component }) => (
           <Route key={path} path={path} component={Component} exact />
         ))}
@@ -17,6 +21,6 @@ const AppRouter = () => {
       <Redirect to={SHOP_ROUTE} />
     </Switch>
   );
-};
+});
 
 export default AppRouter;
